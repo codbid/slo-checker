c980954ba147ced535fd8734da90de04cad438de
Daniil Istomin <istomin.05.3@gmail.com>
2026-04-29 00:07:07 +0700

    Initial commit

---
+++ src/main/java/com/codbid/slo/checker/SloCheckerApplication.java
@@ -0,0 +1,13 @@
+package com.codbid.slo.checker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SloCheckerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SloCheckerApplication.class, args);
+    }
+
+}
