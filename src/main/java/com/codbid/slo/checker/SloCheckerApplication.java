78e835f3ececaee32634fa3adf96a515153d4bfa
Daniil Istomin <istomin.05.3@gmail.com>
2026-04-29 20:04:56 +0700

    feat(slo): add check scheduler

--- src/main/java/com/codbid/slo/checker/SloCheckerApplication.java
+++ src/main/java/com/codbid/slo/checker/SloCheckerApplication.java
@@ -4,7 +4,9 @@ import com.codbid.slo.checker.config.SloProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @EnableConfigurationProperties(SloProperties.class)
 @SpringBootApplication
 public class SloCheckerApplication {
